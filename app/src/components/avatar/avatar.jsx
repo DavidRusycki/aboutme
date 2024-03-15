7fe26ddecdf84d1b237daebafd912d9406f3e9d5
David Rusycki <davidrusyck@gmail.com>
2024-03-14 21:28:34 -0300

    feat: init avatar component

---
+++ app/src/components/avatar/avatar.jsx
@@ -0,0 +1,9 @@
+import './avatar.css'
+
+export default function avatar() {
+    return (
+        <div>
+            <img className="avatar" src="./eu.jpg" alt="personal photo"/>
+        </div>
+    );
+}
--- app/src/components/home.jsx
+++ app/src/components/home.jsx
@@ -1,5 +1,10 @@
+import Avatar from './avatar/avatar.jsx';
+
 export default function Home() {
     return (
-        <h1>Hello world!</h1>
+        <>
+            <h1>Hello world!</h1>
+            <Avatar/>
+        </>
     );
 }
